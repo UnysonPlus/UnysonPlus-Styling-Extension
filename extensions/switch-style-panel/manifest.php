6dab581dbedbd1f0286bb25d776690605189c52c
jonmlas <jonmlas@gmail.com>
2025-08-21 19:05:27 +0800

    Manifest updated

--- extensions/switch-style-panel/manifest.php
+++ extensions/switch-style-panel/manifest.php
@@ -1,11 +1,31 @@
 <?php if ( ! defined( 'FW' ) ) {
-	die( 'Forbidden' );
+    die( 'Forbidden' );
 }
 
 $manifest = array();
 
 $manifest['name']        = __( 'Switch Style Panel', 'fw' );
-$manifest['description'] = __( 'Show on the front-end a panel that allows the user to make the switch between predefined styles.', 'fw' );
-$manifest['version'] = '1.0.0';
-$manifest['display'] = 'styling';
-$manifest['standalone']   = true;
+$manifest['slug']        = 'unysonplus-switch-style-panel';
+$manifest['description'] = __( 
+    'Show on the front-end a panel that allows the user to make the switch between predefined styles.', 
+    'fw' 
+);
+
+$manifest['version']     = '1.0.0';
+$manifest['display']     = true;
+$manifest['standalone']  = true;
+
+// Repo Info
+$manifest['github_update'] = 'UnysonPlus/UnysonPlus-Switch-Style-Panel-Extension';
+$manifest['github_repo']   = 'https://github.com/UnysonPlus/UnysonPlus-Switch-Style-Panel-Extension';
+$manifest['github_branch'] = 'master';
+
+// Author Info
+$manifest['author']     = 'UnysonPlus';
+$manifest['author_uri'] = 'https://www.lastimosa.com.ph/unysonplus';
+
+// Extra Meta
+$manifest['license']      = 'GPL-2.0-or-later';
+$manifest['text_domain']  = 'fw';
+$manifest['requires_php'] = '7.4';
+$manifest['requires_wp']  = '5.8';
--- manifest.php
+++ manifest.php
@@ -1,13 +1,32 @@
 <?php if ( ! defined( 'FW' ) ) {
-	die( 'Forbidden' );
+    die( 'Forbidden' );
 }
 
 $manifest = array();
 
+// Basic Info
 $manifest['name']        = __( 'Styling', 'fw' );
-$manifest['description'] = __( "This extension lets you control the website visual style. Starting from predefined styles to changing specific fonts and colors across the website.", 'fw' );
-$manifest['version']     = '1.0.6';
-$manifest['display']     = true;
-$manifest['standalone']  = true;
+$manifest['slug']        = 'unysonplus-styling';
+$manifest['description'] = __(
+    "This extension lets you control the website visual style. Starting from predefined styles to changing specific fonts and colors across the website.",
+    'fw'
+);
 
-$manifest['github_update'] = 'ThemeFuse/Unyson-Styling-Extension';
+$manifest['version']    = '1.0.6';
+$manifest['display']    = true;
+$manifest['standalone'] = true;
+
+// Repository Info
+$manifest['github_update'] = 'UnysonPlus/UnysonPlus-Styling-Extension';
+$manifest['github_repo']   = 'https://github.com/UnysonPlus/UnysonPlus-Styling-Extension';
+$manifest['github_branch'] = 'master';
+
+// Author Info
+$manifest['author']     = 'UnysonPlus';
+$manifest['author_uri'] = 'https://www.lastimosa.com.ph/unysonplus';
+
+// Meta
+$manifest['license']      = 'GPL-2.0-or-later';
+$manifest['text_domain']  = 'fw';
+$manifest['requires_php'] = '7.4';
+$manifest['requires_wp']  = '5.8';
